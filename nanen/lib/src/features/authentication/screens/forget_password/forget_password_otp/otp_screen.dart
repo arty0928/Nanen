258dd77b9a7f6b0cb04fa061897c3bff5a1664f7
arty0928 <arty0928@konkuk.ac.kr>
2023-04-07 17:56:35 +0900

    OTP (verification code sent by email)

--- nanen/lib/src/features/authentication/screens/forget_password/forget_password_otp/otp_screen.dart
+++ nanen/lib/src/features/authentication/screens/forget_password/forget_password_otp/otp_screen.dart
@@ -13,36 +13,38 @@ class OTPScreen extends StatelessWidget {
       child: Scaffold(
         body: Container(
           padding: const EdgeInsets.all(tDefaultSize),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                tOtpTitle,
-                style: GoogleFonts.montserrat(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 80.0,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  tOtpTitle,
+                  style: GoogleFonts.montserrat(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 80.0,
+                  ),
                 ),
-              ),
-              Text(tOtpSubTitle.toUpperCase(),
-                  style: Theme.of(context).textTheme.titleLarge),
-              const SizedBox(height: 40.0),
-              const Text("${tOtpMessage}support@naver.com",
-                  textAlign: TextAlign.center),
-              const SizedBox(height: 20.0),
-              OtpTextField(
-                numberOfFields: 6,
-                fillColor: Colors.black.withOpacity(0.1),
-                filled: true,
-                onSubmit: (code) {
-                  print("otp is => $code");
-                },
-              ),
-              const SizedBox(height: 20.0),
-              SizedBox(
-                  width: double.infinity,
-                  child: ElevatedButton(
-                      onPressed: () {}, child: const Text(tNext))),
-            ],
+                Text(tOtpSubTitle.toUpperCase(),
+                    style: Theme.of(context).textTheme.titleLarge),
+                const SizedBox(height: 40.0),
+                const Text("${tOtpMessage}support@naver.com",
+                    textAlign: TextAlign.center),
+                const SizedBox(height: 20.0),
+                OtpTextField(
+                  numberOfFields: 6,
+                  fillColor: Colors.black.withOpacity(0.1),
+                  filled: true,
+                  onSubmit: (code) {
+                    print("otp is => $code");
+                  },
+                ),
+                const SizedBox(height: 20.0),
+                SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                        onPressed: () {}, child: const Text(tNext))),
+              ],
+            ),
           ),
         ),
       ),
