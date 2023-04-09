ccde6b49a985e6ec377604412f4147aff3f5c936
Nbowow <nbo5548@naver.com>
2023-04-10 01:53:26 +0900

    04/10

---
+++ toonflix/lib/pages/home_page.dart
@@ -0,0 +1,317 @@
+import 'package:flutter/material.dart';
+import 'package:toonflix/graph/linechart.dart';
+import 'package:toonflix/graph/pricePoints.dart';
+import 'package:toonflix/util/doctor_card.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple[100],
+      body: SafeArea(
+        // top: false,
+        // bottom: false,
+        // right: false,
+        // left: false,
+        // maintainBottomViewPadding: false,
+        child: Column(
+          children: [
+            //app bar
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 25.0, vertical: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'AI를 활용한 마음건강 기록 앱 서비스',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 18),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Text(
+                        'Nanuen',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 24),
+                      ),
+                    ],
+                  ),
+
+                  //setting
+                  Container(
+                    padding: const EdgeInsets.all(5),
+                    decoration: BoxDecoration(
+                      color: Colors.deepPurple[200],
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: const Icon(Icons.settings),
+                  ),
+                ],
+              ),
+            ),
+
+            const SizedBox(
+              height: 25,
+            ),
+
+            //card -> how do you feel?
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  height: 130,
+                  width: 150,
+                  padding: const EdgeInsets.fromLTRB(35, 10, 10, 10),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[200],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: const Row(
+                    children: [
+                      Text(
+                        'AI Model',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  width: 40,
+                ),
+                Container(
+                  height: 130,
+                  width: 150,
+                  padding: const EdgeInsets.all(12),
+                  decoration: BoxDecoration(
+                    color: Colors.deepPurple[200],
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Column(
+                    children: [
+                      const Text(
+                        'THURSDAY',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Text(
+                        '28',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Container(
+                        padding: const EdgeInsets.all(12),
+                        decoration: BoxDecoration(
+                          color: Colors.deepPurple[400],
+                          borderRadius: BorderRadius.circular(12),
+                        ),
+                        child: const Center(
+                            child: Text(
+                          '오늘의 미션',
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        )),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 25,
+            ),
+            //search bar
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple[200],
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: const TextField(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    border: InputBorder.none,
+                    hintText: 'How can we help you?',
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage('lib/images/active.png'),
+                        ),
+                        color: Colors.deepPurple[200],
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: const Row(
+                        children: [],
+                      ),
+                    ),
+                    const Text('Active'),
+                  ],
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage('lib/images/creative.png'),
+                        ),
+                        color: Colors.deepPurple[200],
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: const Row(
+                        children: [],
+                      ),
+                    ),
+                    const Text('Creative'),
+                  ],
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage('lib/images/people.png'),
+                        ),
+                        color: Colors.deepPurple[200],
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: const Row(
+                        children: [],
+                      ),
+                    ),
+                    const Text('People'),
+                  ],
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  children: [
+                    Container(
+                      height: 70,
+                      width: 70,
+                      decoration: BoxDecoration(
+                        image: const DecorationImage(
+                          image: AssetImage('lib/images/calm.png'),
+                        ),
+                        color: Colors.deepPurple[200],
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: const Row(
+                        children: [],
+                      ),
+                    ),
+                    const Text('Calm'),
+                  ],
+                ),
+              ],
+            ),
+
+            //Graph
+            // Container(
+            //   child: LineChartWidget(pricePoints),
+            // ),
+            const SizedBox(
+              height: 20,
+            ),
+
+            Container(
+              child: LineChartWidget(pricePoints),
+            ),
+
+            const SizedBox(
+              height: 20,
+            ),
+
+            //Youtuber
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              child: Text(
+                '명상 크리에이터',
+                textAlign: TextAlign.left,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Expanded(
+                child: ListView(
+              scrollDirection: Axis.horizontal,
+              children: const [
+                DoctorCard(
+                  doctorImagePath: 'lib/images/emindyoga.jpg',
+                  rating: '4.8',
+                  doctorName: '에일린 mind yoga',
+                  doctorProfession: 'Yoga',
+                ),
+                DoctorCard(
+                  doctorImagePath: 'lib/images/yangbro.jpg',
+                  rating: '',
+                  doctorName: '양브로의 정신세계',
+                  doctorProfession: 'Doctor',
+                ),
+                DoctorCard(
+                  doctorImagePath: 'lib/images/brainrich.jpg',
+                  rating: '4.9',
+                  doctorName: '정신과의사 뇌부자들',
+                  doctorProfession: 'Doctor',
+                ),
+              ],
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ toonflix/lib/widgets/line_chart_widget.dart
@@ -0,0 +1,1 @@
+class LineChartWidget extends StatelessWidget {}
