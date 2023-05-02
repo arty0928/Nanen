0c8f64e28accf4f37cf722299fd2f6b289c9b5ea
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-05-02 23:13:52 +0900

    set firebase store

--- lib/src/features/authentication/controllers/signup_controller.dart
+++ lib/src/features/authentication/controllers/signup_controller.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/widgets.dart';
 import 'package:get/get.dart';
+import 'package:nanen/src/features/authentication/models/user_model.dart';
 import 'package:nanen/src/repository/authentication_repository/authentication_repository.dart';
+import 'package:nanen/src/repository/user_repository/user_repository.dart';
+
+import '../screens/forget_password/forget_password_otp/otp_screen.dart';
 
 class SignUpController extends GetxController {
   static SignUpController get instance => Get.find();
@@ -10,6 +14,8 @@ class SignUpController extends GetxController {
   final password = TextEditingController();
   final fullName = TextEditingController();
   final phoneNo = TextEditingController();
+
+  final userRepo = Get.put(UserRepository());
 
   //Call this Function from Design & it will do the rest
   void registerUser(String email, String password) async {
@@ -24,6 +30,12 @@ class SignUpController extends GetxController {
   }
 
   //Get phoneNo from user (Screen) and pass it to Auth Repository for Firebase Authentication
+  Future<void> createUser(UserModel user) async {
+    await userRepo.createUser(user);
+    phoneAuthentication(user.phoneNo);
+    Get.to(() => const OTPScreen());
+  }
+
   void phoneAuthentication(String phoneNo) {
     AuthenticationRepository.instance.phoneAuthentication(phoneNo);
   }
