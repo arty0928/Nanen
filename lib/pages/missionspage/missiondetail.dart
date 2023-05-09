a0a1dd2ac968f2aa85736fafcbf471eb25df1849
koreankdj <koreankdj98@gmail.com>
2023-05-09 11:50:35 +0900

    0509

--- lib/pages/missionspage/missiondetail.dart
+++ lib/pages/missionspage/missiondetail.dart
@@ -1,25 +1,167 @@
 import 'package:flutter/material.dart';
+import 'package:percent_indicator/percent_indicator.dart';
+
+import 'missionimage.dart';
 
 class MissionDetail extends StatelessWidget {
-  const MissionDetail({super.key});
+  final int idx;
+
+  MissionDetail(this.idx, {Key? key}) : super(key: key);
+
+  List missiontexts = [
+    '20분 산책하기',
+    '마트가서 장보기',
+    '달 사진 찍어보기',
+    '좋아하는 음식 포장해오기',
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 10,
-      itemBuilder: (_, index) {
-        return ListTile(
-          title: Text('$index 만큼 산책하기'),
-          leading: Container(
-            width: 50,
-            height: 50,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: Colors.grey.withOpacity(0.5),
+    return Scaffold(
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Container(
+                width: double.maxFinite,
+                height: 150,
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple.shade200,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.menu),
+                      iconSize: 30,
+                    ),
+                    Expanded(child: Container()),
+                    IconButton(
+                      onPressed: () {},
+                      icon: const Icon(Icons.settings),
+                      iconSize: 35,
+                    )
+                  ],
+                ),
+              ),
+              Positioned(
+                left: 120,
+                top: 50,
+                child: Container(
+                  width: 180,
+                  height: 200,
+                  decoration: const BoxDecoration(
+                      shape: BoxShape.circle, color: Colors.white),
+                ),
+              ),
+              const Positioned(
+                  left: 160,
+                  top: 90,
+                  child: Text(
+                    'Active',
+                    style: TextStyle(
+                      fontSize: 35,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ))
+            ],
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 15, right: 15),
+            child: LinearPercentIndicator(
+              lineHeight: 35,
+              // percent 는 실제 데이터에 따라 수정이 필요하다.
+              percent: 0.6,
+              progressColor: Colors.deepPurple.shade200,
+              backgroundColor: Colors.grey.shade300,
+              barRadius: const Radius.circular(15),
             ),
           ),
-        );
-      },
+          Column(
+            children: [
+              Container(
+                height: 90,
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Row(children: [
+                  Container(
+                    height: 70,
+                    width: 70,
+                    margin: const EdgeInsets.only(right: 15),
+                    child: Container(
+                      width: 50,
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                        color: Colors.grey.withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Row(children: [
+                      Text(
+                        missiontexts[idx - 1],
+                        style: const TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                      )
+                    ]),
+                  )
+                ]),
+              ),
+              Container(
+                height: 320,
+                width: 400,
+                margin:
+                    const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                padding: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Colors.deepPurple.shade100,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '기록 방식',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        '산책을 하고 난 후 풍경 사진을 한 장 찍어보세요!',
+                        style: TextStyle(fontSize: 14),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                      Text(
+                        '기록 하기',
+                        style: TextStyle(
+                            fontSize: 15, fontWeight: FontWeight.bold),
+                      ),
+                      MissionImage(),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )
+        ],
+      ),
     );
   }
 }
--- lib/pages/missionspage/missionlist.dart
+++ lib/pages/missionspage/missionlist.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'missiondetail.dart';
+
 class MissionList extends StatefulWidget {
   const MissionList({super.key});
 
@@ -20,46 +22,52 @@ class _MissionListState extends State<MissionList> {
     return Column(
       children: [
         for (int i = 1; i < 5; i++)
-          Container(
-            height: 90,
-            margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-            padding: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10),
-            ),
-            child: Row(children: [
-              Radio(
-                value: "",
-                groupValue: "",
-                activeColor: Colors.deepPurple.shade100,
-                onChanged: (index) {},
+          GestureDetector(
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => MissionDetail(i)));
+            },
+            child: Container(
+              height: 90,
+              margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+              padding: const EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(10),
               ),
-              Container(
-                height: 70,
-                width: 70,
-                margin: const EdgeInsets.only(right: 15),
-                child: Container(
-                  width: 50,
-                  height: 50,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                    color: Colors.grey.withOpacity(0.5),
+              child: Row(children: [
+                Radio(
+                  value: "",
+                  groupValue: "",
+                  activeColor: Colors.deepPurple.shade100,
+                  onChanged: (index) {},
+                ),
+                Container(
+                  height: 70,
+                  width: 70,
+                  margin: const EdgeInsets.only(right: 15),
+                  child: Container(
+                    width: 50,
+                    height: 50,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.grey.withOpacity(0.5),
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 10),
-                child: Row(children: [
-                  Text(
-                    // mission index에 맞게 변경
-                    missiontexts[i - 1],
-                    style: const TextStyle(
-                        fontSize: 16, fontWeight: FontWeight.bold),
-                  )
-                ]),
-              )
-            ]),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 10),
+                  child: Row(children: [
+                    Text(
+                      // mission index에 맞게 변경
+                      missiontexts[i - 1],
+                      style: const TextStyle(
+                          fontSize: 16, fontWeight: FontWeight.bold),
+                    )
+                  ]),
+                )
+              ]),
+            ),
           )
       ],
     );
