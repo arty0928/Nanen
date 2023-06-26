31c797f5c1eb302ac6e72e846cea444e32617fac
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-06-26 14:06:47 +0900

    login 합치기

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:nanen/src/utils/theme/theme.dart';
 
 import 'src/features/core/screens/mainPage.dart';
 
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
