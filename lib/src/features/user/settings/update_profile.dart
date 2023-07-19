37f8edf183471cbc0cc29278dada953d880bc827
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-07-19 16:21:10 +0900

    edit profile page

--- lib/src/features/user/settings/update_profile.dart
+++ lib/src/features/user/settings/update_profile.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:line_awesome_flutter/line_awesome_flutter.dart';
+import 'package:nanen/src/constants/colors.dart';
+import 'package:nanen/src/constants/image_strings.dart';
+import 'package:nanen/src/constants/sizes.dart';
+import 'package:nanen/src/constants/text_strings.dart';
 import 'package:nanen/src/features/user/settings/widget/settingCategory.dart';
 import 'package:nanen/src/features/user/mypage/top/mypageTop.dart';
 import 'package:nanen/src/features/user/mypage/widget/editProfileBtn.dart';
@@ -9,223 +15,223 @@ class UpdateProfileScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: const CostumedAppBar(
-//         bartitle: 'MyPage > Edit Profile',
-//       ),
-//       body: SingleChildScrollView(
-//         child: Container(
-//           padding: const EdgeInsets.all(tDefaultSize),
-//           child: Column(
-//             children: [
-//               Padding(
-//                 padding: const EdgeInsets.all(20.0),
-//                 child: Row(
-//                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                   children: [
-//                     //Profile
-//                     Stack(
-//                       children: [
-//                         SizedBox(
-//                           width: 120,
-//                           height: 120,
-//                           child: ClipRRect(
-//                               borderRadius: BorderRadius.circular(100),
-//                               child: const Image(
-//                                   image: AssetImage(tProfileImage))),
-//                         ),
-//                         Positioned(
-//                           bottom: 0,
-//                           right: 0,
-//                           child: Container(
-//                             width: 35,
-//                             height: 35,
-//                             decoration: BoxDecoration(
-//                                 borderRadius: BorderRadius.circular(100),
-//                                 color: tPrimaryColor),
-//                             child: const Icon(
-//                               LineAwesomeIcons.camera,
-//                               size: 20,
-//                               color: Colors.black,
-//                             ),
-//                           ),
-//                         ),
-//                       ],
-//                     ),
-
-//                     //number
-//                     const Expanded(
-//                       child: Column(
-//                         children: [
-//                           Padding(
-//                             padding: EdgeInsets.symmetric(
-//                                 horizontal: 25, vertical: 10),
-//                             child: Align(
-//                               alignment: Alignment.centerLeft,
-//                               child: Text(
-//                                 'Eunseopark',
-//                                 style: TextStyle(
-//                                     fontWeight: FontWeight.bold, fontSize: 20),
-//                               ),
-//                             ),
-//                           ),
-//                           Row(
-//                             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//                             children: [
-//                               Column(
-//                                 children: [
-//                                   Text(
-//                                     '237',
-//                                     style: TextStyle(
-//                                         fontWeight: FontWeight.bold,
-//                                         fontSize: 18),
-//                                   ),
-//                                   Text('active')
-//                                 ],
-//                               ),
-//                               Column(
-//                                 children: [
-//                                   Text(
-//                                     '237',
-//                                     style: TextStyle(
-//                                         fontWeight: FontWeight.bold,
-//                                         fontSize: 18),
-//                                   ),
-//                                   Text('calm')
-//                                 ],
-//                               ),
-//                               Column(
-//                                 children: [
-//                                   Text(
-//                                     '237',
-//                                     style: TextStyle(
-//                                         fontWeight: FontWeight.bold,
-//                                         fontSize: 18),
-//                                   ),
-//                                   Text('creative')
-//                                 ],
-//                               ),
-//                               Column(
-//                                 children: [
-//                                   Text(
-//                                     '237',
-//                                     style: TextStyle(
-//                                         fontWeight: FontWeight.bold,
-//                                         fontSize: 18),
-//                                   ),
-//                                   Text('people')
-//                                 ],
-//                               ),
-//                             ],
-//                           ),
-//                         ],
-//                       ),
-//                     )
-//                   ],
-//                 ),
-//               ),
-//               const SizedBox(
-//                 height: 50,
-//               ),
-//               Form(
-//                 child: Column(
-//                   children: [
-//                     TextFormField(
-//                       decoration: const InputDecoration(
-//                           label: Text(tFullName),
-//                           prefixIcon: Icon(Icons.person_outline_rounded)),
-//                     ),
-//                     TextFormField(
-//                       decoration: const InputDecoration(
-//                           label: Text(tFullName),
-//                           prefixIcon: Icon(Icons.person_outline_rounded)),
-//                     ),
-//                     TextFormField(
-//                       decoration: const InputDecoration(
-//                           label: Text(tFullName),
-//                           prefixIcon: Icon(Icons.person_outline_rounded)),
-//                     ),
-//                     const SizedBox(
-//                       height: tFormHeight,
-//                     ),
-
-//                     /// - Form Submit Button
-//                     SizedBox(
-//                       width: double.infinity,
-//                       child: ElevatedButton(
-//                         onPressed: () =>
-//                             Get.to(() => const UpdateProfileScreen()),
-//                         style: ElevatedButton.styleFrom(
-//                             backgroundColor: tPrimaryColor,
-//                             side: BorderSide.none,
-//                             shape: const StadiumBorder()),
-//                         child: const Text(
-//                           tEditProfile,
-//                           style: TextStyle(color: tDarkColor),
-//                         ),
-//                       ),
-//                     ),
-//                     const SizedBox(
-//                       height: tFormHeight,
-//                     ),
-//                     Row(
-//                       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//                       children: [
-//                         const Text.rich(
-//                           TextSpan(
-//                             text: tJoined,
-//                             style: TextStyle(fontSize: 12),
-//                             children: [
-//                               TextSpan(
-//                                   text: tJoinedAt,
-//                                   style: TextStyle(
-//                                       fontWeight: FontWeight.bold,
-//                                       fontSize: 12)),
-//                             ],
-//                           ),
-//                         ),
-//                         ElevatedButton(
-//                           onPressed: () {},
-//                           style: ElevatedButton.styleFrom(
-//                               backgroundColor:
-//                                   Colors.redAccent.withOpacity(0.1),
-//                               elevation: 0,
-//                               foregroundColor: Colors.red,
-//                               shape: const StadiumBorder(),
-//                               side: BorderSide.none),
-//                           child: const Text(tDelete),
-//                         ),
-//                       ],
-//                     )
-//                   ],
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
-    return const Scaffold(
-      appBar: CostumedAppBar(
-        bartitle: 'Settings',
+    return Scaffold(
+      appBar: const CostumedAppBar(
+        bartitle: 'MyPage > Edit Profile',
       ),
-      body: Padding(
-        padding: EdgeInsets.all(8.0),
-        child: Column(
-          children: [
-            mypageTop(),
-            EditProfileBtn(),
-            SizedBox(
-              height: 8,
-            ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: const EdgeInsets.all(tDefaultSize),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    //Profile
+                    Stack(
+                      children: [
+                        SizedBox(
+                          width: 120,
+                          height: 120,
+                          child: ClipRRect(
+                              borderRadius: BorderRadius.circular(100),
+                              child: const Image(
+                                  image: AssetImage(tProfileImage))),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          right: 0,
+                          child: Container(
+                            width: 35,
+                            height: 35,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(100),
+                                color: tPrimaryColor),
+                            child: const Icon(
+                              LineAwesomeIcons.camera,
+                              size: 20,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
 
-            // 감정온도
-            MyPageCategory(),
-          ],
+                    //number
+                    const Expanded(
+                      child: Column(
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 25, vertical: 10),
+                            child: Align(
+                              alignment: Alignment.centerLeft,
+                              child: Text(
+                                'Eunseopark',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold, fontSize: 20),
+                              ),
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Column(
+                                children: [
+                                  Text(
+                                    '237',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  ),
+                                  Text('active')
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    '237',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  ),
+                                  Text('calm')
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    '237',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  ),
+                                  Text('creative')
+                                ],
+                              ),
+                              Column(
+                                children: [
+                                  Text(
+                                    '237',
+                                    style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18),
+                                  ),
+                                  Text('people')
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              Form(
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          label: Text(tFullName),
+                          prefixIcon: Icon(Icons.person_outline_rounded)),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          label: Text(tFullName),
+                          prefixIcon: Icon(Icons.person_outline_rounded)),
+                    ),
+                    TextFormField(
+                      decoration: const InputDecoration(
+                          label: Text(tFullName),
+                          prefixIcon: Icon(Icons.person_outline_rounded)),
+                    ),
+                    const SizedBox(
+                      height: tFormHeight,
+                    ),
+
+                    /// - Form Submit Button
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () =>
+                            Get.to(() => const UpdateProfileScreen()),
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: tPrimaryColor,
+                            side: BorderSide.none,
+                            shape: const StadiumBorder()),
+                        child: const Text(
+                          tEditProfile,
+                          style: TextStyle(color: tDarkColor),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: tFormHeight,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        const Text.rich(
+                          TextSpan(
+                            text: tJoined,
+                            style: TextStyle(fontSize: 12),
+                            children: [
+                              TextSpan(
+                                  text: tJoinedAt,
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 12)),
+                            ],
+                          ),
+                        ),
+                        ElevatedButton(
+                          onPressed: () {},
+                          style: ElevatedButton.styleFrom(
+                              backgroundColor:
+                                  Colors.redAccent.withOpacity(0.1),
+                              elevation: 0,
+                              foregroundColor: Colors.red,
+                              shape: const StadiumBorder(),
+                              side: BorderSide.none),
+                          child: const Text(tDelete),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 }
+//     return const Scaffold(
+//       appBar: CostumedAppBar(
+//         bartitle: 'Settings',
+//       ),
+//       body: Padding(
+//         padding: EdgeInsets.all(8.0),
+//         child: Column(
+//           children: [
+//             mypageTop(),
+//             EditProfileBtn(),
+//             SizedBox(
+//               height: 8,
+//             ),
+
+//             // 감정온도
+//             MyPageCategory(),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
