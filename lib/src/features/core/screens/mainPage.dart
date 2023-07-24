81a7ce1b2b9b050836513142bde8d00d5097b809
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-07-24 20:16:04 +0900

    home 수정 0724

--- lib/src/features/core/screens/mainPage.dart
+++ lib/src/features/core/screens/mainPage.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:nanen/src/features/core/screens/HomeTemplate/fitness_app/fitness_app_home_screen.dart';
-import 'package:nanen/src/utils/nav_foot_bar/nav_foot_bar.dart';
+// import 'package:nanen/src/utils/nav_foot_bar/nav_foot_bar.dart';
 
-import '../../user/mypage/mypage.dart';
-import 'Home/home_page.dart';
-import 'calendar/calendar_main.dart';
-import 'misson/screen/mission_screen.dart';
+// import '../../user/mypage/mypage.dart';
+// import 'Home/home_page.dart';
+// import 'calendar/calendar_main.dart';
+// import 'misson/screen/mission_screen.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
