325e25cd0be8ebae762ed53256811eb6272b6295
EunseoPark <88071251+arty0928@users.noreply.github.com>
2023-11-12 23:18:46 +0900

    1112

--- lib/constants/api_consts.dart
+++ lib/constants/api_consts.dart
@@ -1,2 +1,2 @@
 String BASE_URL = 'https://api.openai.com/v1';
-String API_KEY = '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';
+String API_KEY = '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';
